d2f185d9188ca88e3c5996777ded8842918b92d2
wasabina67 <wasabina67@gmail.com>
2024-12-11 11:34:05 +0900

    Update gmailLabelSearch.js

--- gmailLabelSearch.js
+++ gmailLabelSearch.js
@@ -1,1 +1,6 @@
-
+function myFunction() {
+  const targetLabel = 'targetLabel';
+  const notifiedLabel = 'notifiedLabel';
+  const query = 'label:' + targetLabel + ' -label:' + notifiedLabel;
+  const threads = GmailApp.search(query);
+}
