ad1172ecb20326e923c6f8b7ffa06dd87979e8b1
wasabina67 <wasabina67@gmail.com>
2024-12-11 11:48:23 +0900

    Update gmail2discord.js

--- gmail2discord.js
+++ gmail2discord.js
@@ -1,2 +1,10 @@
 function myFunction() {
+  const targetLabel = 'targetLabel';
+  const query = 'label:' + targetLabel;
+  const threads = GmailApp.search(query);
+
+  if (threads.length === 0) {
+    Logger.log('No threads found with the label: ' + targetLabel);
+    return;
+  }
 }
